4f7aa0055b91c29addbe3625edd10c34d5c3d621
Rurik Greenall <rurik.greenall@gmail.com>
2017-05-29 21:22:05 +0200

    Add more examples, update index file

--- src/json-ld-course-server/routes/index.js
+++ src/json-ld-course-server/routes/index.js
@@ -8,16 +8,61 @@ const initialData = {
   introduction: 'This is a short course in JSON-LD',
   tools: [
     {
-      part: 0,
+      part: 1,
       title: 'JSON-LD Playground (running locally)',
       resource: 'json-ld.org/playground/'
     }
   ],
   parts: [
     {
-      part: 0,
-      title: 'Adding context',
-      resource: 'context/a001.jsonld'
+      part: 1,
+      title: 'Exercise one: Adding context',
+      resource: 'json-ld.org/playground/#startTab=tab-expanded&json-ld=http%3A%2F%2Flocalhost%3A3211%2Fdocument%2Fa001'
+    },
+    {
+      part: 2,
+      title: 'Exercise two: Adding context',
+      resource: 'json-ld.org/playground/#startTab=tab-expanded&json-ld=http%3A%2F%2Flocalhost%3A3211%2Fdocument%2Fa002'
+    },
+    {
+      part: 3,
+      title: 'Exercise two: Adding context',
+      resource: 'json-ld.org/playground/#startTab=tab-expanded&json-ld=http%3A%2F%2Flocalhost%3A3211%2Fdocument%2Fa003&context=http%3A%2F%2Flocalhost%3A3211%2Fcontext%2Fa003.jsonld'
+    },
+    {
+      part: 4,
+      title: 'Exercise three: Aliasing',
+      resource: 'json-ld.org/playground/#startTab=tab-compacted&json-ld=http%3A%2F%2Flocalhost%3A3211%2Fdocument%2Fa004.json&context=http%3A%2F%2Flocalhost%3A3211%2Fcontext%2Fa004.jsonld'
+    },
+    {
+      part: 5,
+      title: 'Exercise four: Aliasing',
+      resource: 'json-ld.org/playground/#startTab=tab-compacted&json-ld=http%3A%2F%2Flocalhost%3A3211%2Fdocument%2Fa005.json&context=http%3A%2F%2Flocalhost%3A3211%2Fcontext%2Fa005.jsonld'
+    },
+    {
+      part: 6,
+      title: 'Exercise five: Datatypes',
+      resource: 'json-ld.org/playground/#startTab=tab-expanded&json-ld=http%3A%2F%2Flocalhost%3A3211%2Fdocument%2Fa006'
+    },
+    {
+      part: 7,
+      title: 'Exercise six: Datatypes',
+      resource: 'json-ld.org/playground/#startTab=tab-expanded&json-ld=http%3A%2F%2Flocalhost%3A3211%2Fdocument%2Fa007'
+    },
+    {
+      part: 8,
+      title: 'Exercise five: Datatypes',
+      resource: 'json-ld.org/playground/#startTab=tab-expanded&json-ld=http%3A%2F%2Flocalhost%3A3211%2Fdocument%2Fa008'
+    },
+    {
+      part: 9,
+      title: 'Exercise five: Datatypes (fails)',
+      resource: 'json-ld.org/playground/#startTab=tab-expanded&json-ld=http%3A%2F%2Flocalhost%3A3211%2Fdocument%2Fa009'
+    },
+    {
+      part: 10,
+      title: 'Exercise five: Datatypes',
+      resource: 'json-ld.org/playground/#startTab=tab-expanded&json-ld=http%3A%2F%2Flocalhost%3A3211%2Fdocument%2Fa010'
     }
   ]
 }
@@ -27,7 +72,14 @@ router.get('/', function (req, res, next) {
   res.render('index', initialData);
 });
 
-router.get('/context/:contextFile', function (req, res, next) {
+router.get('/:contextDirectory/:contextFile', function (req, res, next) {
+
+  var json = '.json';
+  var contextFile = req.params.contextFile;
+  var contextDirectory = req.params.contextDirectory;
+
+  var leaf = (contextDirectory === 'document' && contextFile.substring(contextFile.length - json.length) !== json) ?
+  contextFile + json : contextFile;
 
   var options = {
     root: __dirname + '/../public/',
@@ -36,11 +88,12 @@ router.get('/context/:contextFile', function (req, res, next) {
       'Content-Type': 'application/ld+json'
     }
   }
-  res.sendFile('files/context/' + req.params.contextFile, options, function (err) {
+
+  res.sendFile('files/' + contextDirectory + '/' + leaf, options, function (err) {
     if (err) {
       next(err);
     } else {
-      console.log('Sent:', req.params.contextFile);
+      console.log('Sent:', leaf);
     }
   });
 });
